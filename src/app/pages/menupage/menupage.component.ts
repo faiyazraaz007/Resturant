6dc39260f69b28347fe1a72857e1994b609ed244
Faiyaz Ahmed <faiyazsam007@gmail.com>
2022-06-19 15:31:27 +0600

    first comment

---
+++ src/app/pages/menupage/menupage.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
+
+@Component({
+  selector: 'app-menupage',
+  templateUrl: './menupage.component.html',
+  styleUrls: ['./menupage.component.css']
+})
+export class MenupageComponent implements OnInit {
+
+  constructor(private param:ActivatedRoute,private service:OrderDetailsService) { }
+  getMenuId:any;
+  menuData:any;
+
+  ngOnInit(): void {
+    this.getMenuId = this.param.snapshot.paramMap.get('id');
+    console.log(this.getMenuId,'getmenu');
+    if(this.getMenuId)
+    {
+
+      // only one id pass in menuData with filter
+      this.menuData =  this.service.foodDetails.filter((value)=>{
+          return value.id == this.getMenuId;
+        });
+        console.log(this.menuData,'menudata>>');
+        
+    }
+    
+  }
+
+}
